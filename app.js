de32f1d7ccdb36b64c19f50ea4746d7f2768db14
Brian Aung <soekhantaung16@gmail.com>
2022-04-19 16:11:44 +1000

    add seperate handlebars for clinician and patient ui

--- app.js
+++ app.js
@@ -7,28 +7,27 @@ const app = express()
 
 // Configure Handlebars.
 app.engine("hbs", exphbs.engine({
-	defaultlayout: "main",
+	defaultlayout: "patient", // it doesn't work???
 	extname: "hbs"
 }))
-
 app.set("view engine", "hbs")
 app.use(express.static("public"))
 
 // Our "home page" - currently the About Diabetes page.
 app.get('/', (req, res) => {
-    res.render("about_diabetes.hbs")
+  res.render("about_diabetes.hbs", {layout: 'clinician.hbs'})
 })
 
 // Serve up static pages.
 app.get('/about_diabetes.html', (req, res) => {
-    res.render("about_diabetes.hbs")
+  res.render("about_diabetes.hbs", {layout: 'patient.hbs', title: 'About Diabetes'})
 })
 
 app.get('/about_website.html', (req, res) => {
-	res.render("about_website.hbs")
+	res.render("about_website.hbs", {layout: 'patient.hbs', title: 'About Website'})
 })
 
 // Tells the app to listen on port 3000 and logs that information to the console.
 app.listen(process.env.PORT || 3000, () => { 
-    console.log('The library app is running!') 
-  }) 
+  console.log('The library app is running!') 
+}) 
