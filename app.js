5f3ce70791b3247c590bce3d9d909cdd15068426
Brian Aung <soekhantaung16@gmail.com>
2022-04-20 00:06:53 +1000

    smol fixes

--- app.js
+++ app.js
@@ -15,19 +15,28 @@ app.use(express.static("public"))
 
 // Our "home page" - currently the About Diabetes page.
 app.get('/', (req, res) => {
-  res.render("about_diabetes.hbs", {layout: 'patient.hbs'})
+  res.render("about-diabetes.hbs", {layout: 'patient.hbs'})
 })
 
 // Serve up static pages.
-app.get('/about_diabetes.html', (req, res) => {
-  res.render("about_diabetes.hbs", {layout: 'patient.hbs', title: 'About Diabetes'})
+app.get('/about-diabetes.html', (req, res) => {
+  res.render("about-diabetes.hbs", {layout: 'patient.hbs', title: 'About Diabetes'})
 })
 
-app.get('/about_website.html', (req, res) => {
-	res.render("about_website.hbs", {layout: 'patient.hbs', title: 'About Website'})
+app.get('/about-website.html', (req, res) => {
+	res.render("about-website.hbs", {layout: 'patient.hbs', title: 'About Website'})
+})
+
+// other pages
+app.get('/patient-dashboard.html', (req, res) => {
+  res.render("patient-dashboard.hbs", {
+    layout: 'patient.hbs', 
+    title: 'Dashboard',
+    username: 'Alice'
+  })
 })
 
 // Tells the app to listen on port 3000 and logs that information to the console.
 app.listen(process.env.PORT || 3000, () => { 
-  console.log('The library app is running!') 
+  console.log('Diabetes@Home app is running!') 
 }) 
