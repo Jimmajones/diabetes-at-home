522712d12471c61987b360c7b0f8b1af282413c0
James Amanatidis <jamanatidis@student.unimelb.edu.au>
2022-04-21 10:32:02 +1000

    Ran prettifier.

--- app.js
+++ app.js
@@ -1,42 +1,51 @@
 // Import external packages.
 const express = require('express')
-const exphbs = require("express-handlebars")
+const exphbs = require('express-handlebars')
 
 // Set up the application as an Express app.
 const app = express()
 
 // Configure Handlebars.
-app.engine("hbs", exphbs.engine({
-	defaultlayout: "patient", // it doesn't work???
-	extname: "hbs"
-}))
-app.set("view engine", "hbs")
-app.use(express.static("public"))
+app.engine(
+    'hbs',
+    exphbs.engine({
+        defaultlayout: 'patient', // it doesn't work???
+        extname: 'hbs',
+    })
+)
+app.set('view engine', 'hbs')
+app.use(express.static('public'))
 
 // Our "home page" - currently the About Diabetes page.
 app.get('/', (req, res) => {
-  res.render("about-diabetes.hbs", {layout: 'patient.hbs'})
+    res.render('about-diabetes.hbs', { layout: 'patient.hbs' })
 })
 
 // Serve up static pages.
 app.get('/about-diabetes', (req, res) => {
-  res.render("about-diabetes", {layout: 'patient.hbs', title: 'About Diabetes'})
+    res.render('about-diabetes', {
+        layout: 'patient.hbs',
+        title: 'About Diabetes',
+    })
 })
 
 app.get('/about-website', (req, res) => {
-	res.render("about-website", {layout: 'patient.hbs', title: 'About Website'})
+    res.render('about-website', {
+        layout: 'patient.hbs',
+        title: 'About Website',
+    })
 })
 
 // other pages
 app.get('/patient-dashboard', (req, res) => {
-  res.render("patient-dashboard.hbs", {
-    layout: 'patient.hbs', 
-    title: 'Dashboard',
-    username: 'Alice'
-  })
+    res.render('patient-dashboard.hbs', {
+        layout: 'patient.hbs',
+        title: 'Dashboard',
+        username: 'Alice',
+    })
 })
 
 // Tells the app to listen on port 3000 and logs that information to the console.
-app.listen(process.env.PORT || 3000, () => { 
-  console.log('Diabetes@Home app is running!') 
-}) 
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Diabetes@Home app is running!')
+})
--- models/clinicianModel.js
+++ models/clinicianModel.js
@@ -1,16 +1,15 @@
-module.exports =
-[
-	{
-		id: '0',
-		first_name: 'Chris',
-		last_name: 'Ician',
-		patients: ['0', '1']
-	},
-	
-	{
-		id: '1',
-		first_name: 'Gregory',
-		last_name: 'House',
-		patients: ['2']
-	}
+module.exports = [
+    {
+        id: '0',
+        first_name: 'Chris',
+        last_name: 'Ician',
+        patients: ['0', '1'],
+    },
+
+    {
+        id: '1',
+        first_name: 'Gregory',
+        last_name: 'House',
+        patients: ['2'],
+    },
 ]
