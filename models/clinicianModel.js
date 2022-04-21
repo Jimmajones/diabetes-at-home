fb5c34337b2decadb2c69f4ff26caa731c8195b9
James Amanatidis <jamanatidis@student.unimelb.edu.au>
2022-04-21 10:22:34 +1000

    Created an array of sample data instances.

---
+++ models/clinicianModel.js
@@ -0,0 +1,16 @@
+module.exports =
+[
+	{
+		id: '0',
+		first_name: 'Chris',
+		last_name: 'Ician',
+		patients: ['0', '1']
+	},
+	
+	{
+		id: '1',
+		first_name: 'Gregory',
+		last_name: 'House',
+		patients: ['2']
+	}
+]
---
+++ models/patientModel.js
@@ -0,0 +1,26 @@
+module.exports =
+[
+	{
+		id: '0',
+		first_name: 'Pat',
+		last_name: 'Ient',
+		blood_threshold_lower: 1,
+		blood_threshold_upper: 11
+	},
+
+	{
+		id: '1',
+		first_name: 'Tobey',
+		last_name: 'Maguire',
+		blood_threshold_lower: 2,
+		blood_threshold_upper: 10
+	},
+	
+	{
+		id: '2',
+		first_name: 'Slim',
+		last_name: 'Shady',
+		blood_threshold_lower: 3,
+		blood_threshold_upper: 9
+	}
+]
