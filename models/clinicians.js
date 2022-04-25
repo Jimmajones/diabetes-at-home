8bacefd52b78226a378849b5564264f115128587
yijuzhou <yijuzhou@student.unimelb.edu.au>
2022-04-25 20:52:48 +1000

    small changes to clinicians model

--- models/clinicians.js
+++ models/clinicians.js
@@ -4,8 +4,9 @@ const mongoose = require('mongoose')
 const schema = new mongoose.Schema({
     first_name: {type: String, required: true},
     last_name: {type: String, required: true},
-    screen_name: {type: String, required: true},
-    email: {type: String, required: true},
+    screen_name: {type: String, required: true, unique: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
     date_of_birth: {type: Date, required: true},
     bio: String,
     patient_list: [{type: mongoose.Types.ObjectId}]
