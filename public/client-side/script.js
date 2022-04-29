ee4fc2e168d30f9c96f70caeaf1f547d8fd1b7ab
geralo17 <79506777+geralo17@users.noreply.github.com>
2022-04-29 19:15:22 +1000

    added rotation feature and moved script to script file

--- public/client-side/script.js
+++ public/client-side/script.js
@@ -1,8 +1,12 @@
 function toggle(id) {
     var x = document.getElementById(id);
+    var y = document.getElementById("button-" + id)
     if (x.style.display === "none") {
         x.style.display = "block";
+        y.style.transform = "rotate(90deg)";
     } else {
         x.style.display = "none";
+        y.style.transform = "none";
     }
+
 }
