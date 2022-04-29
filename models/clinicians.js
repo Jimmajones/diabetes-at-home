7e78d09be835d79db6b29c4af1bf6057933854f8
James Amanatidis <jamanatidis@student.unimelb.edu.au>
2022-04-29 12:15:30 +1000

    Stripped down models to bare essentials for Deliverable 2.

--- models/clinicians.js
+++ models/clinicians.js
@@ -2,13 +2,8 @@ const mongoose = require('mongoose')
 
 // Define schema with data types
 const schema = new mongoose.Schema({
-  first_name: { type: String, required: true },
-  last_name: { type: String, required: true },
-  screen_name: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  date_of_birth: { type: Date, required: true },
-  bio: String,
+  first_name: String,
+  last_name: String,
   patient_list: [{ type: mongoose.Types.ObjectId }],
 })
 
--- models/patients.js
+++ models/patients.js
@@ -2,69 +2,15 @@ const mongoose = require('mongoose')
 
 // Define schema with data types
 const schema = new mongoose.Schema({
-  first_name: { type: String, required: true },
-  last_name: { type: String, required: true },
-  screen_name: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  date_of_birth: { type: Date, required: true },
-  post_code: Number,
-  bio: { type: String, required: true },
-  engagement_score: Number,
-  leaderboard_rank: Number,
-  clinician_id: { type: mongoose.Types.ObjectId, required: true },
-  required_data: [
-    {
-      data_type: {
-        type: String,
-        enum: [
-          'blood_glucose_level',
-          'weight',
-          'insulin_doses',
-          'exercise_steps',
-        ],
-      },
-      lower_bound: Number,
-      upper_bound: Number,
-    },
-  ],
+  first_name: String,
+  last_name: String,
   daily_data: [
     {
       blood_glucose_level: {
         value: Number,
         comment: String,
-        time: { type: Date, default: Date.now },
-        within_threshold: Boolean,
+        time: Date,
       },
-      weight: {
-        value: Number,
-        comment: String,
-        time: { type: Date, default: Date.now },
-        within_threshold: Boolean,
-      },
-      insulin_doses: {
-        value: Number,
-        comment: String,
-        time: { type: Date, default: Date.now },
-        within_threshold: Boolean,
-      },
-      exercise_steps: {
-        value: Number,
-        comment: String,
-        time: { type: Date, default: Date.now },
-        within_threshold: Boolean,
-      },
-      status: {
-        type: String,
-        enum: [
-          'GOOD',
-          'INCOMPLETE',
-          'OUTSIDE THRESHOLD',
-          'INCOMPLETE & OUTSIDE THRESHOLD',
-        ],
-      },
-      warning_text: String,
-      date_recorded: Date,
     },
   ],
 })
