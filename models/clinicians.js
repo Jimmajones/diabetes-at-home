66ca12610f3eb737ff5fb3835e8c48e37cc7d2d5
James Amanatidis <jamanatidis@student.unimelb.edu.au>
2022-04-29 12:38:10 +1000

    Split up Patient schemas into logical units, specified id type for Clinician.

--- models/clinicians.js
+++ models/clinicians.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const schema = new mongoose.Schema({
   first_name: String,
   last_name: String,
-  patient_list: [{ type: mongoose.Types.ObjectId }],
+  patient_list: [{ type: mongoose.Types.ObjectId, ref: 'Patient' }],
 })
 
 const Clinician = mongoose.model('Clinician', schema)
--- models/patients.js
+++ models/patients.js
@@ -1,19 +1,22 @@
 const mongoose = require('mongoose')
 
-// Define schema with data types
-const schema = new mongoose.Schema({
-  first_name: String,
-  last_name: String,
-  daily_data: [
-    {
-      blood_glucose_level: {
-        value: Number,
-        comment: String,
-        time: Date,
-      },
-    },
-  ],
+const bloodSchema = new mongoose.Schema({
+  value: Number,
+  comment: String,
+  when: { type: Date, default: Date.now },
 })
 
-const Patient = mongoose.model('Patient', schema)
-module.exports = Patient
+const recordSchema = new mongoose.Schema({
+  blood: bloodSchema,
+})
+
+const patientSchema = new mongoose.Schema({
+  first_name: String,
+  last_name: String,
+  daily_data: [recordSchema],
+})
+
+const Blood = mongoose.model('Blood', bloodSchema)
+const Record = mongoose.model('Record', recordSchema)
+const Patient = mongoose.model('Patient', patientSchema)
+module.exports = { Blood, Record, Patient }
