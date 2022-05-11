21d3165eed5f4ead61d0ff92ff376ff9fc37fa55
Brian Aung <soekhantaung16@gmail.com>
2022-05-11 12:53:17 +1000

    change folder name

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,6 @@
-function toggle(id) {
-  var x = document.getElementById(id)
-  var y = document.getElementById('button-' + id)
+const toggle = (id) => {
+  let x = document.getElementById(id)
+  let y = document.getElementById('button-' + id)
   if (x.style.display === 'none') {
     x.style.display = 'block'
     y.style.transform = 'rotate(90deg)'
